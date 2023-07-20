4977a039a7df9b4a6911c3e4d23d94deba042f3b
sithipol lalee <sithipol1@hotmail.com>
2023-07-20 22:01:12 +0700

    update

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Restaurant from "../views/Restaurant.vue";
-import Location from "../views/Location.vue";
 const routes = [
     {
         path: "/",
         name: "Restaurants Map",
         component: Restaurant,
-    },
-    {
-        path: "/search",
-        name: "Restaurants Search",
-        component: Location,
     },
 ];
 
